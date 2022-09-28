696042b42272a643b9c1f53c9bc7a4a61a7c26f9
jvsoarez <joaovictorcpsa@gmail.com>
2022-09-28 00:45:48 -0300

    feat: add function that colors pixels with selected palette color

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const main = document.querySelector('main');
 const elementosClasseColor = document.querySelectorAll('.color');
+const colorBlack = document.querySelector('#colorBlack');
+const colorRed = document.querySelector('#colorRed');
+const colorBlue = document.querySelector('#colorBlue');
+const colorYellow = document.querySelector('#colorYellow');
 
 function insereSectionNaMain() {
   const novaSection = document.createElement('section');
@@ -29,6 +33,27 @@ for (let posicao = 0; posicao < elementosClasseColor.length; posicao += 1) {
       elementosClasseColor[posicao2].classList.remove('selected');
       event.target.classList.add('selected');
     }
+  });
+}
+
+// Colore os pixel com a cor da paleta selecionada
+
+const pixelsComClassePixel = document.getElementsByClassName('pixel');
+
+function trocarCoresPretoEazul(e) {
+  if (colorBlack.className.includes('selected')) e.target.style.backgroundColor = 'black';
+  if (colorRed.className.includes('selected')) e.target.style.backgroundColor = '#9a031e';
+}
+
+function trocarCoresVermelhoEamarelo(e) {
+  if (colorBlue.className.includes('selected')) e.target.style.backgroundColor = '#118ab2';
+  if (colorYellow.className.includes('selected')) e.target.style.backgroundColor = '#ffb703';
+}
+
+for (let posicao = 0; posicao < pixelsComClassePixel.length; posicao += 1) {
+  pixelsComClassePixel[posicao].addEventListener('click', (e) => {
+    trocarCoresPretoEazul(e);
+    trocarCoresVermelhoEamarelo(e);
   });
 }
 

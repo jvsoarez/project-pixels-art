8e0e8d2b6ba59bee59f572dd6841a799f635dc8c
jvsoarez <joaovictorcpsa@gmail.com>
2022-09-28 00:26:14 -0300

    feat: function that changes the selected class to the clicked palette

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-const conteudoPrincipal = document.querySelector('main');
-// const sectionQuadroPixels = document.querySelector('#pixel-board');
-// console.log(conteudoPrincipal);
+const main = document.querySelector('main');
+const elementosClasseColor = document.querySelectorAll('.color');
 
 function insereSectionNaMain() {
   const novaSection = document.createElement('section');
   novaSection.setAttribute('id', 'pixel-board');
-  conteudoPrincipal.appendChild(novaSection);
+  main.appendChild(novaSection);
 }
 
 insereSectionNaMain();
@@ -22,5 +21,17 @@ function insereDivsDoQuadroDePixelNaSection() {
 
 insereDivsDoQuadroDePixelNaSection();
 
+// Troca classe "selected" para a paleta clicada
+
+for (let posicao = 0; posicao < elementosClasseColor.length; posicao += 1) {
+  elementosClasseColor[posicao].addEventListener('click', (event) => {
+    for (let posicao2 = 0; posicao2 < elementosClasseColor.length; posicao2 += 1) {
+      elementosClasseColor[posicao2].classList.remove('selected');
+      event.target.classList.add('selected');
+    }
+  });
+}
+
 // Referências:
 // Método setAttribute -> https://www.educative.io/answers/how-to-add-an-id-to-element-in-javascript
+// Método classList -> https://www.w3schools.com/jsref/prop_element_classlist.asp
